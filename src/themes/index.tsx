d6d1b84717b0c45efdf78ab15915635cf670328c
Seonghyeon Cho <seonghyeoncho96@gmail.com>
2022-10-10 18:59:42 +0900

    Update index.tsx

--- src/themes/index.tsx
+++ src/themes/index.tsx
@@ -1,14 +1,14 @@
 import { createTheme } from "@mui/material/styles";
-import { indigo } from "@mui/material/colors";
+import { purple } from "@mui/material/colors";
 
 const theme = createTheme();
 
 export const basicTheme = createTheme({
   palette: {
     primary: {
-      main: indigo[500],
-      light: indigo[200],
-      dark: indigo[800],
+      main: purple[500],
+      light: purple[200],
+      dark: purple[800],
     },
   },
   typography: {
